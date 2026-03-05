004b36e3de6c3b694d6bbf28c40f49cbe2134f56
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-05 20:05:05 +0200

    shows "Hello, Illia"

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Illia
- * Created on: MAR 2026
+ * Created on: March 2026
  * This program shows "Hello, Illia"
 */
 
-
 basic.showString('Hello, Illia')
-
