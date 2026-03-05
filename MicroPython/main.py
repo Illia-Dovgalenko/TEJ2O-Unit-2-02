5777f1d5c511da38db80790c5bba9c6a71b504bd
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-05 03:24:24 +0000

    python varsion

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,9 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Illia
+Created on: MAR 2026
+This module shows "Hello, Illia"
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.scroll("Hello, Illia")
